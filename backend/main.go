43b48398aabf4318f59ee1f7a7b00eafde110384
Stephen Hinett <stephen.hinett@digital.justice.gov.uk>
2022-05-26 00:00:28 +0100

    update: contains a bunch of new files and folders that will be used later on
    - Added editorconfig just for the current yml files
    - Got docker-compose up and running for the backend api printing Hello,
      Something on the 9000 port
    - Added Go air to recompile the app once Go changes have been spotted

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, Something!"))
+	})
+
+	log.Println("Starting server on :9000")
+	err := http.ListenAndServe(":9000", mux)
+	log.Fatal(err)
+}
