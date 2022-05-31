f19d9a95a5cd31f722ce7ce6ddd4bc87b5f0b84b
Stephen Hinett <stephen.hinett@digital.justice.gov.uk>
2022-05-31 12:43:55 +0100

    updates: to more dockerfiles and added an nginx proxy. Still not got the frontend talking to backend yet tho

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+type response struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, to the backend!"))
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response{Message: "Hello, to the backend!"})
+	})
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response{Message: "Hello, to the backend!"})
 	})
 
 	log.Println("Starting server on :9000")
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect, useState } from 'react'
 
-const App: React.FunctionComponent<{}> = () => (
-    <h1>Hello, to the frontend</h1>
-)
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+const App: React.FunctionComponent<{}> = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:9000/api", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then(
+            res => res.json()
+        ).then(
+            (result) => {
+                console.log(result);
+            },
+            (err) => {
+                setIsLoaded(true)
+                setError(err)
+            }
+        )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className='some-element'>
+                <h1>Hello, to the frontend!</h1>
+                <p>{items.map(item => (item.name))}</p>
+            </div>
+        )
+    }
+}
+
+root.render(<App />)
